3c87bd54da72d0201caeaf8e245d157166d5c5d1
Sai krishna Modhupalli <saikri@Sais-MacBook-Air.local>
2024-07-30 17:24:47 +0530

    Infinite scroll

--- LLD/app/src/components/Body.js
+++ LLD/app/src/components/Body.js
@@ -3,30 +3,37 @@ import { MemeCard } from "./MemeCard";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [memes, setMemes] = useState(null);
+  const [memes, setMemes] = useState([]);
+  const [showShimmer, setShowShimmer] = useState(false);
   useEffect(() => {
     fetchMemes();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+      fetchMemes();
+    }
+  };
+
   const fetchMemes = async () => {
+    setShowShimmer(true);
     const data = await fetch("https://meme-api.com/gimme/20");
     const json = await data.json();
-    console.log(json);
-    setMemes(json.memes);
-    console.log(memes);
+    setShowShimmer(false);
+    setMemes((memes) => [...memes, ...json.memes]);
   };
 
   return (
     <div className="flex flex-wrap">
-      {memes ? (
+      {memes &&
         memes.map((meme, i) => (
           <div key={i}>
             <MemeCard data={meme} />
           </div>
-        ))
-      ) : (
-        <Shimmer />
-      )}
+        ))}
+      {showShimmer && <Shimmer />}
     </div>
   );
 };
