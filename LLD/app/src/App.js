0fa4c80ef2d5925ca5af0163881c548630063bf8
Sai krishna Modhupalli <saikri@Sais-MacBook-Air.local>
2024-07-30 12:16:48 +0530

    routing

--- LLD/app/src/App.js
+++ LLD/app/src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./components/Body";
+import About from "./components/About";
+import Team from "./components/Team";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl underline">Hello world!</h1>
-      <Body />
+      <header className="text-3xl underline bg-black text-white text-center">
+        Hello world!
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/team">Team</a>
+        <a href="/login">Login</a>
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/team" element={<Team />}></Route>
+          </Route>
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ LLD/app/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">About</h1>
+      <p>Lorem epsum sai krishna</p>
+    </div>
+  );
+};
+
+export default About;
