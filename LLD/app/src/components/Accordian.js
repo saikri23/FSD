0f9b812a14091c6cd3d47fbd21d8ac01a72c31a1
Sai krishna Modhupalli <saikri@Sais-MacBook-Air.local>
2024-07-30 20:49:10 +0530

    Accordian with array of items to open commented lines are normal ones

--- LLD/app/src/components/Accordian.js
+++ LLD/app/src/components/Accordian.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import AccordianItem from "./AccordianItem";
 
 const Accordian = () => {
-  const [openIdx, setOpenIdx] = useState(0);
+  // const [openIdx, setOpenIdx] = useState(0);
+  const [openIdx, setOpenIdx] = useState([1, 2]);
 
   const data = [
     {
@@ -21,6 +22,13 @@ const Accordian = () => {
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point ofusing Lorem Ipsum is that it has a more-or-less normal distribution ofletters, as opposed to using 'Content here, content here', making itlook like readable English. Many desktop publishing packages and webpage editors now use Lorem Ipsum as their default model text, and asearch for 'lorem ipsum' will uncover many web sites still in theirinfancy. Various versions have evolved over the years, sometimes byaccident, sometimes on purpose (injected humour and the like).",
     },
   ];
+  const toggleAccordion = (idx) => {
+    setOpenIdx((prevOpenIdxs) =>
+      prevOpenIdxs.includes(idx)
+        ? prevOpenIdxs.filter((openIdx) => openIdx !== idx)
+        : [...prevOpenIdxs, idx]
+    );
+  };
   return (
     <div className="w-[50%] m-auto">
       {data.map((item, Idx) => (
@@ -28,10 +36,12 @@ const Accordian = () => {
           key={Idx}
           title={item.title}
           description={item.description}
-          isOpen={openIdx === Idx}
-          setIsOpen={() =>
-            openIdx === Idx ? setOpenIdx(null) : setOpenIdx(Idx)
-          }
+          // isOpen={openIdx === Idx}
+          // setIsOpen={() =>
+          //   openIdx === Idx ? setOpenIdx(null) : setOpenIdx(Idx)
+          // }
+          isOpen={openIdx.includes(Idx)}
+          setIsOpen={() => toggleAccordion(Idx)}
         />
       ))}
     </div>
